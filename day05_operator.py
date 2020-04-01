a1f787291b1a3ab9ed34fa09fb7e2da31ab23d1a
fan-tian0 <62786120+fan-tian0@users.noreply.github.com>
2020-04-01 21:00:36 +0800

    master

--- day05_operator.py
+++ day05_operator.py
@@ -49,3 +49,107 @@ a = -123
 a %= 10
 print(a)
 print('123')
+
+'''
+    2、比较运算符：bi
+        ==
+        !=
+        >=
+        <=
+        >
+        <
+        is
+        is not
+
+        返回True 或者 False
+
+'''
+
+n1 = (int(input('请输入第一个数：')))
+n2 = (int(input('请输入第二个数：')))
+
+result = n1 > n2
+print('n1 > n2 ',result)
+
+m1 = 'hello'
+m2 = 'hello'
+result = m1 == m2
+print('m1 == m2',result)
+
+#is 对象（变量）的地址进行比较 (判断两个数的内存是否一样) 
+age = 10
+age1 = 10
+print(age is age1) #判断两个内存地址是否一样
+print(id(age))  #打印两个变量的内存地址
+print(id(age1))
+#在终端中，有小整数池(定义：-5~256)这些数是提前建立好的不会被回收的。
+#在大整数池中，没执行一次就创建一次。
+
+'''
+    3、逻辑运算符
+        and 逻辑与  
+        or  逻辑或
+        not 逻辑非  
+    
+    逻辑运算符返回的结果是：Ture / False
+'''
+print(False and True)   #and 两边都是 true, 结果就是true
+print(True and True)    #and 一边是False ，结果就是False
+print(False and False)
+
+print(False or True) # or 一边为True ，就是True
+print(False or False)# or 两边是False，结果就是False
+print(True or True)
+
+print(not False) #not 取反 
+print(not True)
+
+'''
+    4、位运算
+        & 
+        | 
+        ! 
+        ^
+        ~
+        <<
+        >>
+
+    位运算操作的是二进制    1： 是True      0： False
+'''
+print(1 & 2)
+print(1 | 5)    #结论： 1 | 奇数 结果就是奇数本身
+print(1 | 10)   #       1 | 偶数 结果就是偶数本身加1
+
+print(1 ^ 3)
+print(1 << 2)   #左移一位就是乘与一个2
+print(1 >> 2)   #右移一位就是除以一个2
+print(bin(~2))
+a = 15
+print(bin(a)) #bin() 输出二进制数
+a = 0b1111 #0b 开头是二进制
+print(int(a)) # 转换成是十进制输出
+
+a = -5
+print(bin(a)) #负数
+
+a = 0o6351  #0o开头是八进制
+print(bin(a))
+print(a)
+
+a = 0xfae #0x开头的是十六进制
+print(a)
+print(bin(a))
+
+'''
+    5、三目运算符：
+        结果 if 表达式 else 结果
+
+    1、中间为条件判断表达式
+    2、结果是真True 就返回左边的结果
+    3、结果是False 就返回右边的结果
+'''
+
+a = 12
+b = 10
+result = (( a + b ) if (a > b) else (a - b))
+print(result)
